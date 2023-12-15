463440f3c723c22b67e871645a2b4cae5c3fc5db
Maria Luisa <211039617@aluno.unb.br>
2023-12-15 16:54:31 -0300

    :nail_care: feat: estiliza Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { HeaderContainer } from './styles'
+import logo from '../../assets/logo.svg'
+import { Scroll, Timer } from '@phosphor-icons/react'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logo} alt="" />
+      <nav>
+        <NavLink to="" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Historico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
