3ed8b37ea1303750aa9bd241736d3bb7a14384ee
Maria Luisa <211039617@aluno.unb.br>
2023-12-23 14:26:00 -0300

    :sparkles: feat: armazena historico na localStorage

--- src/contexts/CycleContext.tsx
+++ src/contexts/CycleContext.tsx
@@ -1,10 +1,11 @@
-import { createContext, useReducer, useState } from 'react'
+import { createContext, useEffect, useReducer, useState } from 'react'
 import { CycleTask, cyclesReducer } from '../reducers/cycles/reducer'
 import {
   addNewCycleAction,
   finishCurrentCycleAction,
   interruptCurrentCycleAction,
 } from '../reducers/cycles/actions'
+import { differenceInSeconds } from 'date-fns'
 
 interface CreateNewTaskData {
   task: string
@@ -29,22 +30,52 @@ interface CycleContextProviderProps {
 }
 
 export function CycleContextProvider({ children }: CycleContextProviderProps) {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleID: null,
-  })
-
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleID: null,
+    },
+    (initialState) => {
+      const storageStateAsJSON = localStorage.getItem(
+        '@timerFocus: cycleState-1.0.0',
+      )
+      if (storageStateAsJSON) {
+        return JSON.parse(storageStateAsJSON)
+      }
+      return initialState
+    },
+  )
   /**
    * state -> estado atual
    * action -> ação que vai ser executada para modificar o estado
    * dispach -> função que vai disparar a executar a ação(action)
    */
 
-  const [amountSeconds, setAmountSeconds] = useState(0) // armazena o total de segundos que ja se passou desde que a variavel foi criada
-  const { cycles, activeCycleID } = cyclesState
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+    localStorage.setItem('@timerFocus: cycleState-1.0.0', stateJSON)
+  }, [cyclesState]) // salvando no LocalStorage
+  /**
+   * nome especifico
+   * versão
+   */
 
+  const { cycles, activeCycleID } = cyclesState
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleID)
   // encontra o ciclo ativo pelo id, na lista cycles
+
+  const [amountSeconds, setAmountSeconds] = useState(() => {
+    if (activeCycle) {
+      const secondsDifference = differenceInSeconds(
+        new Date(),
+        new Date(activeCycle.startDate),
+      ) // diferença entre duas datas
+
+      return secondsDifference
+    }
+    return 0
+  }) // armazena o total de segundos que ja se passou desde que a variavel foi criada
 
   function markCurrentCycleAsFinished() {
     dispatch(finishCurrentCycleAction())
