4975b475cdeecf631784bfe0243fe473dc531ba1
Maria Luisa <211039617@aluno.unb.br>
2023-12-23 14:30:41 -0300

    :bug: fix: ajusta listagem de historico

--- src/pages/Histoy/index.tsx
+++ src/pages/Histoy/index.tsx
@@ -24,7 +24,7 @@ export function History() {
                 <td>{cycle.task}</td>
                 <td>{cycle.minutes} minutos</td>
                 <td>
-                  {formatDistanceToNow(cycle.startDate, {
+                  {formatDistanceToNow(new Date(cycle.startDate), {
                     addSuffix: true,
                     locale: ptBR,
                   })}
